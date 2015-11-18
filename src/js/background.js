3d2cb555add9a4c0d5636315f69721e056c929bf
paul <paul.christophe6@gmail.com>
2015-11-17 22:08:03 -0500

    understanding chrome storage

--- src/js/background.js
+++ src/js/background.js
@@ -10,11 +10,15 @@ store.subscribe(() =>
 store.dispatch(addAWebsite('www.google.com'));
 // store.dispatch(addAWebsite('www.test.com'));
 
-chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	switch(request.type) {
-		case "dom-loaded":
-			console.log(request.data.myProperty);
-		break;
-	}
-	return true;
+chrome.storage.sync.set({test: true}, () => {
+	console.log('test set');
 });
+
+// chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
+// 	switch(request.type) {
+// 		case "dom-loaded":
+// 			console.log(request.data.myProperty);
+// 		break;
+// 	}
+// 	return true;
+// });
--- src/js/content.js
+++ src/js/content.js
@@ -1,21 +1,21 @@
-// happens on every page
-console.log('content');
-console.log('PAUL ');
-let count = 0;
-window.addEventListener("load", function() {
-		chrome.extension.sendMessage({
-				type: "dom-loaded", 
-				data: {
-						myProperty: "value"
-				}
-		});
-}, true);
-// setInterval(() => {
-// 	console.log(count + 1);
-// 	chrome.extension.sendMessage({
-// 		type: "dom-loaded", 
-// 		data: {
-// 			myProperty: ++count
-// 		}
-// 	});
-// }, 2000);
+import url from 'url';
+
+const urlData = url.parse(window.location.href);
+console.log(urlData);
+console.log(urlData.hostname);
+console.log('paul');
+// let count = 0;
+// window.addEventListener("load", function() {
+// 		chrome.extension.sendMessage({
+// 				type: "hostname", 
+// 				data: {
+// 						myProperty: "value"
+// 				}
+// 		});
+// }, true);
+
+console.log(chrome);
+console.log(chrome.storage);
+chrome.storage.sync.get('test', val => {
+	console.log(val);
+});
