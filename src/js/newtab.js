f065ee391054ae2e2f61dafe8a828bd2f7315b24
paul <paul.christophe6@gmail.com>
2015-11-18 22:14:40 -0500

    adding redux to newtab

--- src/js/newtab.js
+++ src/js/newtab.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NewTab from './containers/NewTab'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(
-  <NewTab />,
-  document.getElementById('mount-point')
-);
+chrome.storage.sync.get('store', store => {
+	ReactDOM.render(
+		<Provider store={store.store}>
+			<NewTab />
+		</Provider>,
+	  document.getElementById('mount-point')
+	);
+});
+
+
+
+
+// import App from './containers/App'
+// import configureStore from './store/configureStore'
+// import 'todomvc-app-css/index.css'
+
+// const store = configureStore()
+
+// render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// )
