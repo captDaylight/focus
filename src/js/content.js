b53d9d4ee9552d14ff0cd1727803c849c1844071
paul <paul.christophe6@gmail.com>
2015-11-25 19:28:11 -0500

    trying to get content posted body

--- src/js/content.js
+++ src/js/content.js
@@ -1,15 +1,25 @@
 import url from 'url';
 import findIndex from 'lodash/array/findIndex';
-
+import $ from 'jquery';
 const urlData = url.parse(window.location.href);
 
 chrome.storage.sync.get('state', state => {
 	const blockedSites = state.state.websites.items;
 	const idxOfSite = findIndex(blockedSites, site => urlData.href.indexOf(site.name));
-
+	console.log(blockedSites);
+	console.log(idxOfSite);
 	if (idxOfSite >= 0) {
-		console.log('index found');
-	} else {
-		console.log('index not found');
+		// console.log('in here');
+		// console.log(document.getElementsByTagName('body'));
+		// console.log(document.getElementsByTagName('div'));
+		document.getElementsByTagName('html').innerHTML = '<body><div style="height:100%;width:100%;position:fixed;background-color:green;"></div></body>';
+
+		// $('html').html('<div style="height:100%;width:100%;position:fixed;background-color:green;"></div>');
 	}
 });
+
+		// window.onload = () => {
+		// 	console.log('page loaded');
+		// 	console.log($('html').html());
+		// 	$('body').html('<div style="height:100%;width:100%;position:fixed;background-color:green;"></div>');			
+		// };
