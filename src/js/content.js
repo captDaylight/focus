82111dd2b6901046afc7bc9159ea7bea0e611f4e
paul <paul.christophe6@gmail.com>
2015-11-25 21:42:10 -0500

    setting body

--- src/js/content.js
+++ src/js/content.js
@@ -1,25 +1,16 @@
 import url from 'url';
 import findIndex from 'lodash/array/findIndex';
-import $ from 'jquery';
+
 const urlData = url.parse(window.location.href);
 
 chrome.storage.sync.get('state', state => {
 	const blockedSites = state.state.websites.items;
 	const idxOfSite = findIndex(blockedSites, site => urlData.href.indexOf(site.name));
-	console.log(blockedSites);
-	console.log(idxOfSite);
 	if (idxOfSite >= 0) {
-		// console.log('in here');
-		// console.log(document.getElementsByTagName('body'));
-		// console.log(document.getElementsByTagName('div'));
-		document.getElementsByTagName('html').innerHTML = '<body><div style="height:100%;width:100%;position:fixed;background-color:green;"></div></body>';
-
-		// $('html').html('<div style="height:100%;width:100%;position:fixed;background-color:green;"></div>');
+		const body = document.createElement('body');
+		const mountPoint = document.createElement('div');
+		mountPoint.id = 'mount-point';
+		body.appendChild(mountPoint);
+		document.getElementsByTagName('html')[0].appendChild(body);
 	}
 });
-
-		// window.onload = () => {
-		// 	console.log('page loaded');
-		// 	console.log($('html').html());
-		// 	$('body').html('<div style="height:100%;width:100%;position:fixed;background-color:green;"></div>');			
-		// };
