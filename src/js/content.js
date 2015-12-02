56d749ec319cc95e8c1e4227b1b7a01bee891b48
paul <paul.christophe6@gmail.com>
2015-12-01 22:09:05 -0500

    store connected to the content script

--- src/js/content.js
+++ src/js/content.js
@@ -1,8 +1,12 @@
-import url from 'url';
-import findIndex from 'lodash/array/findIndex';
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import url from 'url';
+import findIndex from 'lodash/array/findIndex';
+import rootReducer from './reducers';
+import Content from './containers/Content';
 const urlData = url.parse(window.location.href);
 
 chrome.storage.sync.get('state', data => {
@@ -11,20 +15,27 @@ chrome.storage.sync.get('state', data => {
 	const idxOfSite = findIndex(blockedSites, checkIfIsSite);
 	const date = data.state.timer.date;
 
+	// set up store
+	const createAndComposeStore = compose(
+		applyMiddleware(thunkMiddleware)
+	)(createStore);
+
+	const store = createAndComposeStore(rootReducer, data.state);
+
 	if (date) {
 		if (date > Date.now() && idxOfSite >= 0) {
+			// set up mount point
 			const body = document.createElement('body');
 			const mountPoint = document.createElement('div');
 			mountPoint.id = 'mount-point';
 			mountPoint.style.cssText = 'color:black;position:fixed;width:100%;height:100%;background-color:green;top:0px;left:0px;z-index:10000;'
 			body.appendChild(mountPoint);
 			document.getElementsByTagName('html')[0].appendChild(body);
-
+		
 			ReactDOM.render(
-				<div>
-					Focus
-					{data.state.timer.date}
-				</div>,
+				<Provider store={store}>
+					<Content />
+				</Provider>,
 			  document.getElementById('mount-point')
 			);
 		}
