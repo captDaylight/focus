e36200418f554a7bb2817068b8f99c9031cfb13f
paul <paul.christophe6@gmail.com>
2015-12-19 17:32:03 -0500

    starting whether today or not

--- src/js/components/SessionsList.js
+++ src/js/components/SessionsList.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import formatAMPM from '../utils/formatAMPM';
 
+function isTodayOrDate(time) {
+	const date = new Date(time);
+	const day = new Date(date.getYear(), date.getMonth(), date.getDay());
+	const now = Date.now();
+	return 
+}
+
 export default function SessionsList(props) {
 	const { sessions } = props;
-	console.log(sessions);
 	return (
 		<div>
 			<h5>WORK SESSIONS</h5>
@@ -15,7 +21,11 @@ export default function SessionsList(props) {
 
 						return (
 							<li key={idx} className={classnames({current: isCurrent})}>
-								{`${formatAMPM(session.date, true)}`} - {`${formatAMPM(session.date + session.duration, true)}`} 
+								{`${formatAMPM(session.date, true)}`} 
+								--
+								{`${formatAMPM(session.date + session.duration, true)}`} 
+								::
+								{isToday()}
 								
 								{isCurrent ? ' CURRENT': null}
 							</li>
