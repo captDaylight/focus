a430f2ee85bea955be94f0dbf089fe46782d5d46
paul <paul.christophe6@gmail.com>
2015-12-22 23:24:28 -0600

    wrap action into todos

--- src/js/components/Todos.js
+++ src/js/components/Todos.js
@@ -3,16 +3,19 @@ import Formsy, { Form } from 'formsy-react';
 import classnames from 'classnames';
 import FocusInput from './FocusInput';
 
-function handleSubmit(e) {
-	console.log(e);
+function wrapSubmit(action) {
+	return function handleSubmit(e) {
+		console.log(e, action, 'yup');
+	}
 }
 
 export default function SessionsList(props) {
+	const { addTodo } = props;
 	return (
 		<div>
 			<h5>TODOS</h5>
 			
-			<Form onSubmit={handleSubmit}>
+			<Form onSubmit={wrapSubmit(addTodo)}>
 				<FocusInput name="website" placeholder="Add a Todo" />
 				<button>Submit</button>
 			</Form>
