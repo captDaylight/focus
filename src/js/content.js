d2dd096c8e55f8d3e102cc41b8bdd6842e0f14b0
paul <paul.christophe6@gmail.com>
2016-01-20 18:29:54 -0500

    updating content page just to show time and working on todo

--- src/js/content.js
+++ src/js/content.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import $ from 'jquery';
 import url from 'url';
+import template from 'lodash/string/template';
+import filter from 'lodash/collection/filter';
 import FocusContainer from './containers/FocusContainer';
 import checkShouldBlock from './utils/checkShouldBlock';
 
 let mounted = false;
+
+const todoItem = template('<li>{todo}</li>');
+const mountTemplate = template(' \
+	<div id="content-container"> \
+		<div id="content-time"><%= minutes %> : <%= seconds%></div> \
+		<div id="content-todo"> \
+			<ul> \
+				<% todos.map(function (todo) {%> <li><%= todo.todo %></li><% }); %> \
+			</ul> \
+		</div> \
+	</div> \
+');
 
 function mountOrNot(siteChecker) {
 	return (siteList, date, state) => {
@@ -32,6 +45,9 @@ chrome.storage.sync.get('state', data => {
 		if (msg.type === 'STATE_UPDATE') {
 			const { websites, timer } = msg.data;
 			checkShouldMountOrNot(websites.websites, timer.date, msg.data);
+			if (mounted) {
+				updateBlocker(msg.data);	
+			}
 		}
 	});
 });
@@ -40,23 +56,29 @@ function mountBlocker(state) {
 	// set up mount point
 	const body = document.body ? document.body : document.createElement('body');
 	const mountPoint = document.createElement('div');
-	mountPoint.id = 'mount-point';
-	mountPoint.style.cssText = 'color:black;position:fixed;width:100%;height:100%;background-color:green;top:0px;left:0px;z-index:10000;'
+	mountPoint.id = 'mount-point-focus';
+	mountPoint.style.cssText = ' \
+		color:black; position:fixed; width:100%; height:100%; \
+		background-color:green; top:0px; left:0px; z-index:10000;';
 	body.appendChild(mountPoint);
 	document.getElementsByTagName('html')[0].appendChild(body);
-
-	ReactDOM.render(
-		<FocusContainer state={state} />,
-	  document.getElementById('mount-point')
-	);
+	
+	updateBlocker(state);
 
 	mounted = true;
 }
 
 function dismountBlocker() {
 	// const body = document.body ? document.body : document.createElement('body');
-	const mountPoint = document.getElementById('mount-point');
+	const mountPoint = document.getElementById('mount-point-focus');
 
 	mountPoint.parentNode.removeChild(mountPoint);
 	mounted = false;
+}
+
+function updateBlocker(data) {
+	const {minutes, seconds} = data.timer;
+		const todos = filter(data.todos.todos, todo => todo.workingOn );
+		console.log(todos);
+	$('#mount-point-focus').html(mountTemplate({minutes, seconds, todos}));
 }
