71acc42c438d6529cb4e1ce8a5bb95f16187bf4a
paul <paul.christophe6@gmail.com>
2016-02-05 20:55:22 -0500

    break sessions up by days

--- src/js/components/SessionsList.js
+++ src/js/components/SessionsList.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import filter from 'lodash/collection/filter';
 import SessionItem from './SessionItem';
+import groupBy from 'lodash/collection/groupBy';
 
+const DAY = 86400000;
 
 function betweenDates(begin, end) {
 	return date => date > begin && date < end;
@@ -16,40 +18,55 @@ export default function SessionsList(props) {
 	const date = new Date();
 	const midnight = date.setHours(0,0,0,0);
 
-	const todaysSessions = filter(sessions, session => session.date > midnight);
+	const sessionDays = groupBy(sessions.reverse(), session => {
+		return Math.floor(session.date / DAY)
+	});
+	
+	console.log(Object.keys(sessionDays).map(time => time * DAY));
+
+
 
 	return (
 		<div id="sessions-container">
 			<h5>WORK LOG</h5>
 			<ul id="sessions-list">
 				{
-					todaysSessions.reverse().map((session, idx) => {
-						const { date, duration } = session;
-						const dateEnd = date + duration;
-						const sessionCheck = betweenDates(date, dateEnd);
-						const current = sessionCheck(now);
 
-						const working = filter(startedTodos, todo => {
-							const { workingOn, completed } = todo;
+					Object.keys(sessionDays).reverse().map((day, idx) => {
+						const sessions = sessionDays[day].map((session, idx) => {
+							const { date, duration } = session;
+							const dateEnd = date + duration;
+							const sessionCheck = betweenDates(date, dateEnd);
+							const current = sessionCheck(now);
+
+							const working = filter(startedTodos, todo => {
+								const { workingOn, completed } = todo;
+								return (
+									workingOn < dateEnd 
+									&& (completed ? completed > dateEnd: true ));
+							});
+							const finished = filter(startedTodos, todo => {
+								const { completed } = todo;
+								return sessionCheck(completed);
+							});
+							
 							return (
-								workingOn < dateEnd 
-								&& (completed ? completed > dateEnd: true ));
-						});
-						const finished = filter(startedTodos, todo => {
-							const { completed } = todo;
-							return sessionCheck(completed);
-						});
+								<SessionItem 
+									key={idx}
+									current={current}
+									date={date}
+									dateEnd={dateEnd}
+									working={working}
+									finished={finished}
+								/>
+							)
+						})
 						
-						return (
-							<SessionItem 
-								current={current}
-								date={date}
-								dateEnd={dateEnd}
-								working={working}
-								finished={finished}
-							/>
-						)
+						return [(
+							<li>----------- {day}</li>
+						), ...sessions];
 					})
+
 				}
 			</ul>
 		</div>
