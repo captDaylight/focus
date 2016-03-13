0ad1113246ccbb1ab75f5d85e8523946e378ffda
paul <paul.christophe6@gmail.com>
2016-03-13 13:48:55 -0400

    focus blocking sites after website schema change

--- src/js/utils/checkShouldBlock.js
+++ src/js/utils/checkShouldBlock.js
@@ -2,6 +2,6 @@ import findIndex from 'lodash/array/findIndex';
 
 export default function checkShouldBlock(urlData) {
 	return (sites) => {
-		return findIndex(sites, site => urlData.href.indexOf(site.name) > -1) >= 0;
+		return findIndex(sites, site => urlData.href.indexOf(site.url) > -1) >= 0;
 	};
 }
