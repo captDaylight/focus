108400a9844c7f615df2a16614f61436fc3afe7d
paul <paul.christophe6@gmail.com>
2017-02-28 15:29:22 -0800

    refactoring reducers

--- src/js/reducers/todos.js
+++ src/js/reducers/todos.js
@@ -9,6 +9,7 @@ import {
   TOGGLE_TODO_EDIT,
   EDIT_TODO,
   UPDATE_TODOS,
+  UPDATE_TODO_ORDER,
 } from '../actions/todos';
 
 const initialState = {
@@ -18,56 +19,60 @@ const initialState = {
 function updateInArray(items, id, keysAndFns) {
   const idx = findIndex(items, item => item.id === id);
   const splitItems = split(items, idx);
-  const  itemPrevious = items[idx];
-  const itemUpdate = {...itemPrevious};
-  keysAndFns.map(obj => itemUpdate[obj.key] = obj.fn(itemPrevious[obj.key]));
+  const itemPrevious = items[idx];
+  const itemUpdate = { ...itemPrevious };
+  keysAndFns.forEach((obj) => {
+    itemUpdate[obj.key] = obj.fn(itemPrevious[obj.key]);
+  });
   return [...splitItems[0], itemUpdate, ...splitItems[1]];
 }
 
-const dateOrNull = value => value ? null : Date.now();
+const dateOrNull = value => (value ? null : Date.now());
 
-export default function todos(state=initialState, action) {
-  switch(action.type) {
-    case ADD_TODO:
+export default function todos(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TODO: {
       const todo = {
         todo: action.todo,
         id: shortid.generate(),
         created: Date.now(),
         completed: null,
-        workingOn: null, 
+        workingOn: null,
         editing: false,
       };
-      return {...state, todos: [...state.todos, todo]};
+      return { ...state, todos: [...state.todos, todo] };
+    }
 
-    case REMOVE_TODO:
+    case REMOVE_TODO: {
       const id = action.id;
-      return {...state, todos: state.todos.filter(item => item.id !== id)};
+      return { ...state, todos: state.todos.filter(item => item.id !== id) };
+    }
 
-    case TOGGLE_TODO_COMPLETION: 
+    case TOGGLE_TODO_COMPLETION:
       return {
         ...state,
         todos: updateInArray(state.todos, action.id, [{
-          key: 'completed', 
+          key: 'completed',
           fn: dateOrNull,
-        }])
+        }]),
       };
 
-    case TOGGLE_TODO_WORKING: 
+    case TOGGLE_TODO_WORKING:
       return {
         ...state,
         todos: updateInArray(state.todos, action.id, [{
-          key: 'workingOn', 
+          key: 'workingOn',
           fn: dateOrNull,
-        }])
+        }]),
       };
 
     case TOGGLE_TODO_EDIT:
       return {
         ...state,
         todos: updateInArray(state.todos, action.id, [{
-          key: 'editing', 
+          key: 'editing',
           fn: value => !value,
-        }])
+        }]),
       };
 
     case EDIT_TODO:
@@ -75,15 +80,18 @@ export default function todos(state=initialState, action) {
         ...state,
         todos: updateInArray(state.todos, action.id, [{
           key: 'todo',
-          fn: value => action.todo
+          fn: () => action.todo,
         }, {
-          key: 'editing', 
+          key: 'editing',
           fn: value => !value,
-        }])
+        }]),
       };
 
     case UPDATE_TODOS:
-      return {...state, todos: action.todos};
+      return { ...state, todos: action.todos };
+
+    case UPDATE_TODO_ORDER:
+      return state;
 
     default:
       return state;
