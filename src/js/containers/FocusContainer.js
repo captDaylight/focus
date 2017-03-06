5e6333fcb6bd2ca2f54675c7fee48a72bd16b11f
paul <paul.christophe6@gmail.com>
2017-03-06 11:58:51 -0500

    some container refactoring

--- src/js/containers/FocusContainer.js
+++ src/js/containers/FocusContainer.js
@@ -83,66 +83,74 @@ export default class FocusContainer extends Component {
     return (
       <section
         id="focus-container"
-        className={classnames({ focusing: !!minutes })}>
+        className={classnames({ focusing: !!minutes })}
+      >
         {(() => {
           switch (ui.introStep) {
             case 0: return (
-                <div>
-                  <AddWebsites
-                    websitesData={websitesData}
-                    addWebsite={actions.addWebsite}
-                    removeWebsite={actions.removeWebsite}
-                    websites={websites}
-                    setNextIntroStep={setNextIntroStep} />
-                </div>
-              );
+              <div>
+                <AddWebsites
+                  websitesData={websitesData}
+                  addWebsite={actions.addWebsite}
+                  removeWebsite={actions.removeWebsite}
+                  websites={websites}
+                  setNextIntroStep={setNextIntroStep}
+                />
+              </div>);
             case 1: return (
-                <div>
-                  <div className="intro-text">
-                    <h5>Step 2 <br/> Add Custom Sites to Block</h5>
-                    <h6>When you are on a site you want to block, click the drop down and select "Block This Site". <br/><br/> You can also start a focus session from here.</h6>
-                  </div>
-                  <img src="dist/img/block.png" />
-                  <div className="intro-next">
-                    <button className="popup" onClick={() => {setNextIntroStep()}}>next</button>
-                  </div>
+              <div>
+                <div className="intro-text">
+                  <h5>Step 2 <br /> Add Custom Sites to Block</h5>
+                  <h6>
+                    When you are on a site you want to block, click the drop down
+                    and select &quot;Block This Site&quot;.
+                    <br /><br />
+                    You can also start a focus session from here.
+                  </h6>
                 </div>
-              );
+                <img alt="" src="dist/img/block.png" />
+                <div className="intro-next">
+                  <button className="popup" onClick={() => setNextIntroStep()}>next</button>
+                </div>
+              </div>);
             case 2: return (
-                <div>
-                  <div className="intro-text">
-                    <h5>Step 3 <br/> Start Focusing</h5>
-                    <h6>A focus session lasts for 25 minutes. The site will go red and countdown until you can take a break.</h6>
-                  </div>
-                  <img className="intro-image" src="dist/img/25m.png" />
-                  <div className="intro-next">
-                    <button className="popup" onClick={() => {setNextIntroStep()}}>next</button>
-                  </div>
+              <div>
+                <div className="intro-text">
+                  <h5>Step 3 <br /> Start Focusing</h5>
+                  <h6>
+                    A focus session lasts for 25 minutes. The site will go red
+                    and countdown until you can take a break.
+                  </h6>
                 </div>
-              );
+                <img alt="" className="intro-image" src="dist/img/25m.png" />
+                <div className="intro-next">
+                  <button className="popup" onClick={() => setNextIntroStep()}>next</button>
+                </div>
+              </div>);
             case 3: return (
-                <div>
-                  <div className="intro-text">
-                    <h5>Step 3 <br/> Add Todos</h5>
-                    <h6>Break your task into the smallest units possible and write them out. Cross them off as you go.</h6>
-                  </div>
-                  <img className="intro-image" src="dist/img/todo.png" />
-                  <div className="intro-next">
-                    <button className="popup" onClick={() => {setNextIntroStep()}}>next</button>
-                  </div>
+              <div>
+                <div className="intro-text">
+                  <h5>Step 3 <br /> Add Todos</h5>
+                  <h6>
+                    Break your task into the smallest units possible and write
+                    them out. Cross them off as you go.
+                  </h6>
                 </div>
-              );
+                <img alt="" className="intro-image" src="dist/img/todo.png" />
+                <div className="intro-next">
+                  <button className="popup" onClick={() => setNextIntroStep()}>next</button>
+                </div>
+              </div>);
             case 4: return (
-                <div>
-                  <div className="intro-text">
-                    <h5>Step 4 <br/> Enjoy!</h5>
-                    <h6>{`That's it, now give it spin!`}</h6>
-                  </div>
-                  <div className="intro-next">
-                    <button className="popup" onClick={() => {setNextIntroStep()}}>next</button>
-                  </div>
+              <div>
+                <div className="intro-text">
+                  <h5>Step 4 <br /> Enjoy!</h5>
+                  <h6>{'That\'s it, now give it spin!'}</h6>
                 </div>
-              );
+                <div className="intro-next">
+                  <button className="popup" onClick={() => setNextIntroStep()}>next</button>
+                </div>
+              </div>);
             default: return (
               <div>
                 <div id="header">
