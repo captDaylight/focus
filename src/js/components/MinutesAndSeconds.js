d433932408e760c14b4a0b40d1a003a31fce41fb
paul <paul.christophe6@gmail.com>
2017-11-17 19:56:25 -0500

    show hours too and minutes etc :)

--- src/js/components/MinutesAndSeconds.js
+++ src/js/components/MinutesAndSeconds.js
@@ -1,11 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+const MINUTE = 60000;
+
+function displayHoursAndMinutes(minutes) {
+  if (minutes < 60) {
+    return (<span>{minutes}</span>);
+  }
+
+  const hours = Math.floor(minutes / 60);
+  const newMinutes = (minutes % 60);
+  return (<span>{hours} : {newMinutes.toString().length < 2 ? `0${newMinutes}` : newMinutes}</span>);
+}
 
 export default function MinutesAndSeconds(props) {
   const { minutes, seconds, toggleAskCancelTimer, askCancelTimer, clearTimer } = props;
 
   return (
     <div id="clock">
-      <span>{minutes}</span>
+      {
+        displayHoursAndMinutes(minutes)
+      }
       <span> : </span>
       <span>{seconds}</span>
     </div>
