a86e07ccbff241c00fe488af1329ffdf9bc7153e
paul <paul.christophe6@gmail.com>
2018-02-19 22:41:23 -0500

    update some copy

--- src/js/components/Versions.js
+++ src/js/components/Versions.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default {
   1.89: (
     <div>
-      HI THERE yopyoyoyo
+      <h1>Updates</h1>
+      <ul>
+        <li>any length focus time</li>
+        <li>count up distractions</li>
+        <li>show the total focus time</li>
+        <li>small design bugs</li>
+      </ul>
     </div>
   ),
 };
