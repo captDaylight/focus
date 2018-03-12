aedc17179e2eefd1beadfd7ad20368bb21fa9619
paul <paul.christophe6@gmail.com>
2018-03-12 13:51:33 +0100

    small version updates

--- src/js/components/Versions.js
+++ src/js/components/Versions.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default {
   1.89: (
     <div>
-      <h1>Focus Updates</h1>
-      <ul>
-        <li>Focus for longer than 60 minutes</li>
-        <li>Display the total amount of time focused</li>
-        <li>Display how many times distractions were blocked</li>
-        <li>And a variety of small design improvements</li>
+      <h2 className="margin-bottom">Focus Updates</h2>
+      <ul className="margin-bottom">
+        <li><h4>- Focus for longer than 60 minutes</h4></li>
+        <li><h4>- Display the total daily time focused</h4></li>
+        <li><h4>- Display how many times distractions were blocked</h4></li>
+        <li><h4>- And a variety of small design improvements</h4></li>
       </ul>
     </div>
   ),
