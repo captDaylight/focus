c040a59aeb0e799e394ab87f9c8be54e2c400cfe
paul <paul.christophe6@gmail.com>
2018-03-14 21:59:28 -0400

    small last minute changes

--- src/js/components/Versions.js
+++ src/js/components/Versions.js
@@ -2,14 +2,24 @@ import React from 'react';
 
 export default {
   1.89: (
-    <div>
+    <div className="version">
       <h2 className="margin-bottom">Focus Updates</h2>
       <ul className="margin-bottom">
         <li><h4>- Focus for longer than 60 minutes</h4></li>
-        <li><h4>- Display the total daily time focused</h4></li>
-        <li><h4>- Display how many times distractions were blocked</h4></li>
+        <li><h4>- Show total time focused</h4></li>
+        <li><h4>- Show number of distractions blocked</h4></li>
         <li><h4>- And a variety of small design improvements</h4></li>
       </ul>
+
+      <div>
+        <a
+          href="https://bit.ly/extensionfocus"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Leave a review and share!
+        </a>
+      </div>
     </div>
   ),
 };
